1bd5fb11de41e3e21b8c45e6ae441fb68d9752d3
Sai Krishna <46342902+d-saikrishna@users.noreply.github.com>
2022-11-09 11:57:20 +0530

    Create RF_Imputation.py
    Code to impute missing values in the dataset using Random Forest Imputation method.

---
+++ Pre-Modelling/RF_Imputation.py
@@ -0,0 +1,66 @@
+import numpy as np
+import sklearn.neighbors._base
+import sys
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+from missingpy import MissForest
+import os
+import pandas as pd
+
+cwd = os.getcwd()
+data_directory = cwd.replace('Pre-Modelling','')+r'InputData/'
+
+MASTER_2018 = pd.read_csv(data_directory+'SEM_Input_2018.csv')
+one_hot_encoded_data = pd.get_dummies(MASTER_2018, columns = ['assam_soil', 'assam_lith','land use'],
+                                     dummy_na=True,drop_first=True)
+
+# Ensuring OHE data represents Null values.
+one_hot_encoded_data.loc[one_hot_encoded_data.assam_soil_nan == 1,
+
+                         [  # "assam_soil_clay, loamy clay, sandy clay, silty clay", reference
+                             "assam_soil_loamy sand, sand",
+                             "assam_soil_rocky, other non-soil categories",
+                             "assam_soil_loam, silt loam, silt, sandy loam"]] = np.nan
+
+del one_hot_encoded_data["assam_soil_nan"]
+
+one_hot_encoded_data.loc[one_hot_encoded_data.assam_lith_nan == 1,
+
+                         [  # "assam_lith_metamorphic, mesozoic and paleozoic intusive", reference
+                             "assam_lith_neogene sedimentary rock",
+                             "assam_lith_paleogene sedementary rock",
+                             "assam_lith_paleozoic rock",
+                             "assam_lith_undeveloped precambrian rock",
+                             "assam_lith_quaternary sediments",
+                             "assam_lith_tertiary sedimentary rocks",
+                             "assam_lith_water"
+                         ]] = np.nan
+
+del one_hot_encoded_data["assam_lith_nan"]
+
+one_hot_encoded_data.loc[one_hot_encoded_data['land use_nan'] == 1,
+
+                         [  # "land use_bare ground",reference
+                             "land use_built",
+                             "land use_range land",
+                             "land use_vegetation",
+                             "land use_water"
+                         ]] = np.nan
+del one_hot_encoded_data["land use_nan"]
+
+X = one_hot_encoded_data.drop(['ID','yr'],axis=1)
+X = X.replace('U/A', np.nan)
+
+imputer = MissForest(random_state=1337) #miss forest
+X_imputed = imputer.fit_transform(X, cat_vars=np.array(range(74,88,1)))
+X_imputed = pd.DataFrame(X_imputed, columns=X.columns)
+X_imputed.to_csv(data_directory+'SEM_X_Imputed.csv')
+
+
+from sklearn.preprocessing import StandardScaler
+cat_columns = np.array(range(74,88,1))
+X_cat = X_imputed.iloc[:,74:]
+X_con = X_imputed.iloc[:,:74]
+
+X_std_con = StandardScaler().fit_transform(X_con)
+X_std = pd.concat([X_std_con.reset_index(drop=True),X_cat.reset_index(drop=True)],axis=1)
+X_std.to_csv(data_directory+'SEM_X_Imputed_Std.csv')
